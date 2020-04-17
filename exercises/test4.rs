58b251c97af672ae33d92434c29c6f54737f7507
Victor Campos <vhscampos@gmail.com>
2020-04-17 12:33:34 +0100

    17 April 2020

--- exercises/test4.rs
+++ exercises/test4.rs
@@ -5,7 +5,12 @@
 
 // Write a macro that passes the test! No hints this time, you can do it!
 
-// I AM NOT DONE
+//
+macro_rules! my_macro {
+    ($val:expr) => {
+        "Hello ".to_owned() + $val
+    };
+}
 
 #[cfg(test)]
 mod tests {
